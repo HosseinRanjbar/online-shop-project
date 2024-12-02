177fe9eaa50ec8027c49465c06a715777407069e
Sahab Ranjbar <amirisahab@gmail.com>
2024-12-02 19:00:36 +0330

    [MODIFY] - add classNames to elements

--- vite-project/src/components/ProductCard/ProductCard.tsx
+++ vite-project/src/components/ProductCard/ProductCard.tsx
@@ -1,5 +1,7 @@
 import { IProduct } from '../../utils/types';
 import './assets/ProductCard.css';
+import AddToCartButton from './components/AddToCartButton';
+import ProductCardWrapper from './components/ProductCardWrapper';
 
 const ProductCard = ({
   name,
@@ -8,15 +10,21 @@ const ProductCard = ({
   description,
   category
 }: IProduct) => {
-  
+
+
   return (
-    <div className='product-card-container'>
-      <img src={images[0]}/>
-      <span>{category?.name}</span>
-      <h3>{name}</h3> 
-      <p>{description}</p>
-      <h4>{price}</h4>
-    </div>
+    <ProductCardWrapper>
+      <>
+        <div className='product-card-container' >
+          <img src={images[0]} />
+          <span className='product-category'>{category?.name}</span>
+          <h3 className='product-name'>{name}</h3>
+          <p className='product-description'>{description}</p>
+          <h4 className='product-price'>$ {price?.toFixed(2)}</h4>
+        </div>
+        <AddToCartButton />
+      </>
+    </ProductCardWrapper>
   )
 }
 
